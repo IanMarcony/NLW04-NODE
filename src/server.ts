e3e1c9ee0f9049bb26a6b56e8037594360c9fd13
IanMarcony <imarconyls@gmail.com>
2021-02-22 18:45:49 -0400

    Initial Project

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "./routes";
+
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(PORT, () =>
+  console.log(`Server is Running at http://localhost:${PORT}`)
+);
